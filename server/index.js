99532618047bed14528f79b3a2d0dd3589bd2c9a
d-rh <derekrhammitt@gmail.com>
2018-08-24 13:56:22 -0500

    Render error template for bad path
    Update error template to include link to homepage and sign in page

--- server/index.js
+++ server/index.js
@@ -9,13 +9,19 @@ mongoose.connect( process.env.DB_URI, { useNewUrlParser: true } );
 app.set('view engine', 'pug');
 
 
-app.get('/', (request, response) => {
-  response.render('index', { title : 'Welcome' })
+app.get('/', (req, res) => {
+  res.render('index', { title : 'Welcome' })
 })
-app.get('/sign', (request, response) => {
-  response.render('sign')
+app.get('/sign', (req, res) => {
+  res.render('sign')
 })
-
+app.post('/sign', (req, res) => {
+  res.render('welcome')
+})
+app.use( (req, res, next) => {
+  res.status(404)
+  res.render('error')
+})
 
 
 

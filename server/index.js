53d3910b2fbb7b80fff0349b05cb0e6aba3b49df
d-rh <derekrhammitt@gmail.com>
2018-08-26 19:02:53 -0500

    add font and mount static path for public assets
    Include Stylish as header font, Open Sans as body font. Plan to
    build out "Guest" sign in and "Friend" sign in with different
    privileges and access. Maybe use sessionStorage for guests and
    MongoDB for storing friends?

--- server/index.js
+++ server/index.js
@@ -8,20 +8,17 @@ mongoose.connect( process.env.DB_URI, { useNewUrlParser: true } );
 
 app.set('view engine', 'pug');
 
-
 app.get('/', (req, res) => {
   res.render('index', { title : 'Welcome' })
 })
 app.get('/sign', (req, res) => {
   res.render('sign', { title : 'Sign In' })
 })
-
 app.get('/welcome', (req, res) => {
   res.render('welcome', {title : 'Welcome!'})
 })
 
-
-
+app.use('/static', express.static('public'))
 app.use( (req, res, next) => {
   res.status(404)
   res.render('error')

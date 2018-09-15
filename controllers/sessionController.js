1fb2412e5fa02913cedf8c7db53ac413de4a2cd1
d-rh <derekrhammitt@gmail.com>
2018-09-15 11:28:14 -0500

    Merge

---
+++ controllers/sessionController.js
@@ -0,0 +1,20 @@
+const Session = require('../models/session');
+
+exports.sessionCreatePost = user => new Promise( (resolve, reject) => {
+  // mongoDB generates _id field when a new
+  // document is inserted and it omits
+  // the _id field
+  const session = new Session({
+
+  });
+
+  session.save()
+    .then((doc) => {
+      console.log(doc);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  resolve(session)
+});

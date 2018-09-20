fead08ff5e38116f93d9d3f767b8968b76f6d4c1
d-rh <derekrhammitt@gmail.com>
2018-09-19 21:41:29 -0500

    Fix sessionController after merge

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -1,40 +1,27 @@
-<<<<<<< HEAD
-const Session = require('../models/session');
-
-exports.sessionCreatePost = user => new Promise( (resolve, reject) => {
-=======
-// ~/controllers/sessionControllers.js
-
 const Session = require('../models/session');
 
 exports.sessionCreatePost = match => new Promise( (resolve, reject) => {
->>>>>>> ca2d342de637c9ea4cfc43d4f3f3756e534e9446
+
   // mongoDB generates _id field when a new
   // document is inserted and it omits
   // the _id field
   const session = new Session({
-<<<<<<< HEAD
 
-=======
     username: match.username
->>>>>>> ca2d342de637c9ea4cfc43d4f3f3756e534e9446
   });
 
   session.save()
     .then((doc) => {
-<<<<<<< HEAD
+
       console.log(doc);
-=======
+
       resolve(doc);
->>>>>>> ca2d342de637c9ea4cfc43d4f3f3756e534e9446
+
     })
     .catch((err) => {
       console.error(err);
     });
-<<<<<<< HEAD
 
-  resolve(session)
+    resolve(session)
 });
-=======
-});
->>>>>>> ca2d342de637c9ea4cfc43d4f3f3756e534e9446
+

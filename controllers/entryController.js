bb675d573e4458f9f16bdf9409bb0d05300b2ba9
dhammitt <derekrhammitt@gmail.com>
2018-10-24 21:25:14 -0500

    Remove unused date check function from entryController

--- controllers/entryController.js
+++ controllers/entryController.js
@@ -27,33 +27,6 @@ exports.getRecentEntries = () => {
     const query = Entry.find().limit(10);
     resolve(query);
     if (err) reject(err);
-    // const query = Entry.where({});
-    // let recentEntries = [];
-
-    // try {
-    //   return query.find((err, entries) => {
-    //     if (err) reject(err);
-    //     console.log("out of database", entries.length);
-    //     entries.forEach(entry => {
-    //       recentPostCheck(entry.date) ? recentEntries.unshift(entry) : "";
-    //     });
-    //     console.log("post filters", recentEntries.length);
-    //     resolve(recentEntries);
-    //   });
-    // } catch (err) {
-    //   console.error(err);
-    //   reject(err);
-    // }
   });
 };
-// const recentPostCheck = entryDate => {
-//   const currentDate = new Date();
-//   const currentTime = currentDate.getTime();
-//   const entryTime = entryDate.getTime();
-//   // last ten days
-//   if (currentTime <= entryTime + 864000000) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
+

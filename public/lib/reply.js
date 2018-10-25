d06dd6dcdf467d4d1958bbe508880af98ce83f8e
dhammitt <derekrhammitt@gmail.com>
2018-10-24 23:22:16 -0500

    Clean up public/lib/reply.js event listener

--- public/lib/reply.js
+++ public/lib/reply.js
@@ -5,27 +5,33 @@ window.onload = () => {
       el.addEventListener('click', () => {
         el.setAttribute('id', 'eventTarget');
         const replyTargetId = el.parentNode.id;
-        replyTargetContent = document.getElementById(replyTargetId);
         const replyForm = document.createElement('form');
+        const replyInput = document.createElement('input');
+        const hiddenInput = document.createElement('input')
+        const replySubmit = document.createElement('button');
+        
+        replyTargetContentEl = document.getElementById(replyTargetId);
+        
         replyForm.setAttribute('method', 'post');
         replyForm.setAttribute('action', '/feed')
-        const replyInput = document.createElement('input');
+        
         replyInput.setAttribute('type', 'text');
         replyInput.setAttribute('name', 'newReply');
         replyInput.setAttribute('class', 'input');
-        const hiddenInput = document.createElement('input')
+        
         hiddenInput.setAttribute('type', 'hidden')
         hiddenInput.setAttribute('name', 'entryId')
         hiddenInput.setAttribute('value', replyTargetId)
-        const replySubmit = document.createElement('button');
+        
         replySubmit.setAttribute('type', 'submit');
         replySubmit.setAttribute('class', 'button is-primary');
         replySubmit.innerHTML = 'Submit';
+        
         replyForm.appendChild(replyInput);
         replyForm.appendChild(hiddenInput);
         replyForm.appendChild(replySubmit);
-        replyTargetContent.appendChild(replyForm)
-        replyTargetContent.removeChild(eventTarget)
+        replyTargetContentEl.appendChild(replyForm)
+        replyTargetContentEl.removeChild(eventTarget)
       })
     })
   }
